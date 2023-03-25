02c27e7048ad2e48f40291ae0b1b0eda5af6c8f8
asenanov7 <asenanov7@gmail.com>
2023-03-26 00:03:38 +0300

    Add Firebase implementations

--- app/src/main/java/com/example/familyalarm/presentation/MainActivity.kt
+++ app/src/main/java/com/example/familyalarm/presentation/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.familyalarm.presentation
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.familyalarm.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,8 +12,16 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    private  val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        auth.createUserWithEmailAndPassword("test@gmail.com", "123456")
+            .addOnSuccessListener { Log.d("ARSEN", "onCreate: success") }
+            .addOnFailureListener { Log.d("ARSEN", "Fail $it")}
     }
 }
